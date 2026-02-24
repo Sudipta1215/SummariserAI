8f6e5c97c12809f30af19bade2ec3a9a16b41b44
Sudipta1215 <sudiptaray1215@gmail.com>
2026-02-24 18:47:28 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import {
   Sparkles, Loader2, History, AlertCircle
 } from 'lucide-react';
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://summariserai2.onrender.com";
 
 const Dashboard = () => {
   const navigate = useNavigate();
